f49a8b0377f7df268eba34a8279082ab8a097705
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2025-09-01 13:29:42 +0300

    LESSON-22 Third point / generator

--- src/main/java/ru/bashkeev/main/Main.java
+++ src/main/java/ru/bashkeev/main/Main.java
@@ -12,14 +12,21 @@ public class Main implements Cloneable {
                 .map(x -> x * 10)
                 .filter(x -> x > 100)
                 .filter(x -> x > 150)
-                .collect(ArrayList::new, (list, item) -> list.add(item));
-        System.out.println(ints);
+                .collect(ArrayList::new, List::add);
+        System.out.println("List<String> strs: " + ints);
+
+        List<Integer> generatedInts = DataStream.of(() -> List.of("100", "200", "300", "400").iterator())
+                .map(Integer::parseInt)
+                .filter(x -> x > 150)
+                .filter(x -> x < 400)
+                .collect(ArrayList::new, List::add);
+        System.out.println("List<Integer> generatedInts: " + generatedInts);
 
         Integer sum = DataStream.of(strs)
                 .map(Integer::parseInt)
                 .map(x -> x / 10)
                 .map(x -> x * 10)
-                .reduce(0, (a, b) -> a + b);
+                .reduce(0, Integer::sum);
         System.out.println("Reduce sum: " + sum);
     }
 
@@ -34,16 +41,24 @@ public class Main implements Cloneable {
 }
 
 class DataStream<T> {
-    private List<T> elements;
-    private List<Function> functions = new ArrayList<>();
-    private List<Predicate<T>> predicates = new ArrayList<>();
+    private Supplier<Iterator<T>> dataSupplier;
+    private List<Function>        functions  = new ArrayList<>();
+    private List<Predicate<T>>    predicates = new ArrayList<>();
+
+    private DataStream(Supplier<Iterator<T>> dataSupplier) {
+        this.dataSupplier = dataSupplier;
+    }
 
     private DataStream(List<T> elements) {
-        this.elements = elements;
+        this.dataSupplier = () -> elements.iterator();
     }
 
     public static <T> DataStream<T> of(List<T> elements) {
         return new DataStream<>(elements);
+    }
+
+    public static <T> DataStream<T> of(Supplier<Iterator<T>> dataSupplier) {
+        return new DataStream<>(dataSupplier);
     }
 
     public <R> DataStream<R> map(Function<T, R> applier) {
@@ -58,35 +73,25 @@ class DataStream<T> {
 
     public <P> P collect(Supplier<P> creator, BiConsumer<P, T> putter) {
         P collection = creator.get();
-        for (T element : elements) {
-            Object processed = element;
-
-            for (Function function : functions) {
-                processed = function.apply(processed);
-            }
-
-            boolean passedAllFilters = true;
-            for (Predicate<T> predicate : predicates) {
-                @SuppressWarnings("unchecked")
-                T castedProcessed = (T) processed;
-                if (!predicate.test(castedProcessed)) {
-                    passedAllFilters = false;
-                    break;
-                }
-            }
-
-            if (passedAllFilters) {
-                @SuppressWarnings("unchecked")
-                T finalResult = (T) processed;
-                putter.accept(collection, finalResult);
-            }
-        }
+        processElements(element -> putter.accept(collection, element));
         return collection;
     }
 
     public T reduce(T identity, BinaryOperator<T> accumulator) {
-        T result = identity;
-        for (T element : elements) {
+        T[] resultHolder = (T[]) new Object[1];
+        resultHolder[0] = identity;
+
+        processElements(element ->
+                resultHolder[0] = accumulator.apply(resultHolder[0], element));
+
+        return resultHolder[0];
+    }
+
+    private void processElements(Consumer<T> action) {
+        Iterator<T> iterator = dataSupplier.get();
+
+        while (iterator.hasNext()) {
+            T element = iterator.next();
             Object processed = element;
 
             for (Function function : functions) {
@@ -106,9 +111,8 @@ class DataStream<T> {
             if (passedAllFilters) {
                 @SuppressWarnings("unchecked")
                 T finalResult = (T) processed;
-                result = accumulator.apply(result, finalResult);
+                action.accept(finalResult);
             }
         }
-        return result;
     }
 }
