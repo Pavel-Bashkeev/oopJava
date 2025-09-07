b7cafd408adcbb660df3b8ae5bc691a06a05c23c
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2025-09-07 11:48:45 +0300

    CHAPTER-7 Practice 7.1.3

--- src/main/java/ru/bashkeev/arithmetic/FractionGenerator.java
+++ src/main/java/ru/bashkeev/arithmetic/FractionGenerator.java
@@ -1,11 +1,27 @@
 package ru.bashkeev.arithmetic;
 
 public class FractionGenerator {
+    private static FractionGenerator instance;
+
+    private FractionGenerator() {
+    }
+
+    public static FractionGenerator getInstance() {
+        if (instance == null) {
+            instance = new FractionGenerator();
+        }
+
+        return instance;
+    }
+
     public Fraction createFraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new ArithmeticException("Знаменатель не может быть 0");
+        }
         return new Fraction(numerator, denominator);
     }
 
-    public Fraction createFraction(int numerator) {
-        return new Fraction(numerator, 1);
+    public Fraction createFraction(int wholeNumber) {
+        return new Fraction(wholeNumber);
     }
 }
