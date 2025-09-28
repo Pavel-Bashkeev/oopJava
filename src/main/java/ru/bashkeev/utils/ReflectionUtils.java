5cbb879daf1fd08dd80f0457b92482b54e2ce42b
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2025-09-29 00:34:37 +0300

    HOMEWORK-15 8.1.4 Validation methods

--- src/main/java/ru/bashkeev/utils/ReflectionUtils.java
+++ src/main/java/ru/bashkeev/utils/ReflectionUtils.java
@@ -1,6 +1,8 @@
 package ru.bashkeev.utils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -18,5 +20,30 @@ public class ReflectionUtils {
         }
 
         return Collections.unmodifiableList(allFields);
+    }
+
+    public static List<Method> validationMethods(Class<?> clazz) {
+        List<Method> validationMethods = new ArrayList<>();
+        Class<?> currentClass = clazz;
+
+        while (currentClass != null && currentClass != Object.class) {
+            Method[] declaredMethods = currentClass.getDeclaredMethods();
+            for (Method method : declaredMethods) {
+                if (isValidationMethod(method)) {
+                    validationMethods.add(method);
+                }
+            }
+            currentClass = currentClass.getSuperclass();
+        }
+
+        return Collections.unmodifiableList(validationMethods);
+    }
+
+    private static boolean isValidationMethod(Method method) {
+        return method.getName().startsWith("test") &&
+                Modifier.isStatic(method.getModifiers()) &&
+                method.getReturnType() == void.class &&
+                method.getParameterCount() == 1 &&
+                method.getParameterTypes()[0] == Object.class;
     }
 }
